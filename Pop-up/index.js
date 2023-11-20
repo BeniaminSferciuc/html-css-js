da72b4282711273c4d2b11afbdd64d36ac696a98
Beniamin Sferciuc <sbt.beniamin@gmail.com>
2023-11-20 21:55:28 +0200

    Add Pop-up

---
+++ Pop-up/index.js
@@ -0,0 +1,9 @@
+const section = document.getElementById("section");
+const overlay = document.getElementById("overlay");
+const showBtn = document.getElementById("show-modal");
+const closeBtn = document.getElementById("close-btn");
+
+showBtn.addEventListener("click", () => section.classList.add("active"));
+overlay.addEventListener("click", () => section.classList.remove("active"));
+
+closeBtn.addEventListener("click", () => section.classList.remove("active"));
